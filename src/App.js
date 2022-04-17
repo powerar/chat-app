5171dcfada46bc43b292ff935a32145d272eefe1
powerar <arpower@bell.net>
2022-04-17 11:43:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from 'stream-chat-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = StreamChat.getInstance('dz5f4d5kzrue');
+const userToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY29sZC1ib251cy01IiwiZXhwIjoxNjUwMjI3NjUxfQ.89hLdE682IMxmAP3dx6pzHF0JPU3fqEf-1sh-SVBcBE';
+
+chatClient.connectUser(
+  {
+    id: 'cold-bonus-5',
+    name: 'cold',
+    image: 'https://getstream.io/random_png/?id=cold-bonus-5&name=cold',
+  },
+
+  userToken
+);
+
+const channel = chatClient.channel('messaging', 'custom_channel_id', {
+  image: 'https://www.drupal.org/files/project-images/react.png',
+  name: 'Talk about React',
+  members: ['cold-bonus-5'],
+});
+
+const App = () => (
+
+  
+  <Chat client={chatClient} theme="messaging light">
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
